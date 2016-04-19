2a221c19b9cf253b2c60e31f28f4478c15637778
letvargo <letvargo@gmail.com>
2016-04-18 18:38:17 -0700

    Add public to bridging functions, update podspec to 0.0.2

--- LVGUtilities.podspec
+++ LVGUtilities.podspec
@@ -1,7 +1,7 @@
 Pod::Spec.new do |s|
 
   s.name         = "LVGUtilities"
-  s.version      = "0.0.1"
+  s.version      = "0.0.2"
   s.summary      = "Basic Swift utility functions."
   s.homepage     = 'https://github.com/letvargo/'
   s.description  = <<-DESC
@@ -13,7 +13,7 @@ A group of basic Swift utility functions for performing the following tasks:
   s.author             = { "Aaron Rasmussen" => "letvargo@gmail.com" }
   s.ios.deployment_target = "8.0"
   s.osx.deployment_target = "10.10"
-  s.source       = { :git => "/Users/doofnugget/Documents/projects/Podspecs/LVGUtilities", :tag => "0.0.1" }
+  s.source       = { :git => "/Users/doofnugget/Documents/projects/Podspecs/LVGUtilities", :tag => "0.0.2" }
   s.source_files  = "Source/**/*"
   s.requires_arc = true
 
--- Source/CBridgingFunctions.swift
+++ Source/CBridgingFunctions.swift
@@ -6,18 +6,18 @@
 //  Copyright © 2016 letvargo. All rights reserved.
 //
 
-func bridge<T: AnyObject>(obj: T) -> UnsafePointer<Void> {
+public func bridge<T: AnyObject>(obj: T) -> UnsafePointer<Void> {
     return UnsafePointer(Unmanaged.passUnretained(obj).toOpaque())
 }
 
-func bridge<T : AnyObject>(ptr: UnsafePointer<Void>) -> T {
+public func bridge<T : AnyObject>(ptr: UnsafePointer<Void>) -> T {
     return Unmanaged<T>.fromOpaque(COpaquePointer(ptr)).takeUnretainedValue()
 }
 
-func bridgeRetained<T: AnyObject>(obj: T) -> UnsafePointer<Void> {
+public func bridgeRetained<T: AnyObject>(obj: T) -> UnsafePointer<Void> {
     return UnsafePointer(Unmanaged.passRetained(obj).toOpaque())
 }
 
-func bridgeTransfer<T: AnyObject>(ptr: UnsafePointer<Void>) -> T {
+public func bridgeTransfer<T: AnyObject>(ptr: UnsafePointer<Void>) -> T {
     return Unmanaged<T>.fromOpaque(COpaquePointer(ptr)).takeRetainedValue()
 }
