abd46e58a5ea81a5a1662aafc4b0be2a79162c0f
letvargo <letvargo@gmail.com>
2016-04-21 05:43:14 -0700

    Update test project

---
+++ Tests/Pods/Target Support Files/Pods-Tests-OSX/Pods-Tests-OSX-umbrella.h
@@ -0,0 +1,6 @@
+#import <Cocoa/Cocoa.h>
+
+
+FOUNDATION_EXPORT double Pods_Tests_OSXVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Tests_OSXVersionString[];
+
---
+++ Tests/Pods/Target Support Files/Pods-Tests-iOS-LVGUtilities/Pods-Tests-iOS-LVGUtilities-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double LVGUtilitiesVersionNumber;
+FOUNDATION_EXPORT const unsigned char LVGUtilitiesVersionString[];
+
