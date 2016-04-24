43f67720c39ac7db9e3e8b1dd77e3f76c54bbf9e
letvargo <letvargo@gmail.com>
2016-04-24 03:57:09 -0700

    Add documentation to FourCharCodeUtilities.swift

--- Source/FourCharCodeUtilities.swift
+++ Source/FourCharCodeUtilities.swift
@@ -29,11 +29,35 @@ import Foundation
  
  **Properties:**
  
- var codeString: String? { get }
+     var codeString: String? { get }
+ 
+ A `String`, exactly 4 characters in length, that represents
+ the 'FourCharCode' of the value.
  
  */
 
 public protocol CodeStringConvertible {
+    
+    /**
+     
+     A `String`, exactly 4 characters in length, that represents
+     the 'FourCharCode' of the value.
+     
+     Many Apple APIs have error codes or constants that can be converted
+     to a 4-character string. Valid 4-character code strings are exactly
+     4 characers long and every character is between ASCII values 32..<127.
+     
+     For example, the constant `kAudioServicesBadPropertySizeError` defined in
+     Audio Toolbox has a 4-character code string '!siz'. Many `OSStatus` error
+     codes have 4-character code strings associated with them, and these
+     can be useful for debugging.
+     
+     If the value that this is called on is not exactly 4 bytes in size,
+     or if any of the bytes (interpreted as a `UInt8`) does not represent an
+     ASCII value contained in `32..<127`, the `codeString` property will be `nil`.
+     
+     */
+    
     var codeString: String? { get }
 }
 
@@ -107,8 +131,21 @@ extension UInt32: CodeStringConvertible { }
 // MARK: String - Extension adding the code property
 
 extension String {
+
+    /**
+     
+     Converts a 4-character string to a `UInt32` value.
+     
+     This property is the reverse of the `codeString` property defined
+     in `CodeStringConvertible`.
+     
+     - precondition: `self.unicodeScalars.count == 4` where every scalar
+     has a value in the range `32..<127`. If these conditions are not met
+     the function will return `nil`.
+     
+     */
     
-    public var code: UInt32? {
+    public func propertyCode() -> UInt32? {
         
         func pointsToUInt32(points: UnsafePointer<UInt8>) -> UInt32 {
             return UnsafePointer<UInt32>(points).memory
@@ -129,5 +166,41 @@ extension String {
         }
         
         return pointsToUInt32(codePoints)
+    }
+    
+    /**
+     
+     Converts a 4-character string to an `Int32` value.
+     
+     This property is the reverse of the `codeString` property defined
+     in `CodeStringConvertible`.
+     
+     - precondition: `self.unicodeScalars.count == 4` where every scalar
+     has a value in the range `32..<127`. If these conditions are not met
+     the function will return `nil`.
+     
+     */
+    
+    public func statusCode() -> OSStatus? {
+        
+        func pointsToInt32(points: UnsafePointer<UInt8>) -> Int32 {
+            return UnsafePointer<Int32>(points).memory
+        }
+        
+        let codePoints = Array(self.utf8.reverse())
+        
+        guard
+            codePoints.count == 4
+            else {
+                return nil
+        }
+        
+        guard
+            codePoints.reduce(true, combine: { $0 && (32..<127).contains($1) })
+            else {
+                return nil
+        }
+        
+        return pointsToInt32(codePoints)
     }
 }
