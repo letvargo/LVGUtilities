29ebe4a365dcec4d4bcb046739644fe3ab214f3c
letvargo <letvargo@gmail.com>
2016-04-24 02:56:41 -0700

    Revised documentation

--- Source/CBridgingFunctions.swift
+++ Source/CBridgingFunctions.swift
@@ -6,17 +6,83 @@
 //  Copyright © 2016 letvargo. All rights reserved.
 //
 
-public func bridge<T: AnyObject>(obj: T) -> UnsafePointer<Void> {
-    return UnsafePointer(Unmanaged.passUnretained(obj).toOpaque())
+/**
+
+ Return an `UnsafePointer<Void>` to any class object.
+ 
+ This function does not retain a reference to the object. If you need to retain
+ a reference to the object to ensure that it is not destroyed, use
+ `bridgeRetained(_:)` instead.
+ 
+ - parameter object: The the object that you need a point to. It must be a
+ class object that conforms to `AnyObject`.
+ 
+ - returns: An `UnsafePointer<Void>` to `object`.
+ 
+ */
+
+public func bridge<T: AnyObject>(object: T) -> UnsafePointer<Void> {
+    return UnsafePointer(Unmanaged.passUnretained(object).toOpaque())
 }
 
-public func bridge<T : AnyObject>(ptr: UnsafePointer<Void>) -> T {
-    return Unmanaged<T>.fromOpaque(COpaquePointer(ptr)).takeUnretainedValue()
+/**
+
+ Cast an `UnsafePointer<Void>` to an object of type `T`.
+ 
+ This function does not consume a reference to the object. If you need to 
+ consume a reference to the object to prevent a retain cyle, use
+ `bridgeTransfer(_:)` instead.
+ 
+ Note that when calling this function the compiler may not be able to infer
+ the type that you are casting to. Use a type annotation to specify the type
+ that you expect.
+ 
+ - parameter pointer: The `UnsafePointer<Void>` that you wish to cast.
+ 
+ - returns: An object of type `T`.
+ 
+ */
+
+public func bridge<T : AnyObject>(pointer: UnsafePointer<Void>) -> T {
+    return Unmanaged<T>.fromOpaque(COpaquePointer(pointer)).takeUnretainedValue()
 }
+
+/**
+ 
+ Return an `UnsafePointer<Void>` to any class object and increases the object's
+ reference count by one.
+ 
+ This function retains a reference to the object. If you do not want to retain
+ a reference to the object, use `bridge(_:)` instead.
+ 
+ - parameter object: The the object that you need a point to. It must be a
+ class object that conforms to `AnyObject`.
+ 
+ - returns: An `UnsafePointer<Void>` to `object`.
+ 
+ */
 
 public func bridgeRetained<T: AnyObject>(obj: T) -> UnsafePointer<Void> {
     return UnsafePointer(Unmanaged.passRetained(obj).toOpaque())
 }
+
+/**
+ 
+ Cast an `UnsafePointer<Void>` to an object of type `T` and consume
+ a reference to the object.
+ 
+ This function consumes a reference to the object. If you do not want to
+ consume a reference to the object use `bridge(_:)` instead.
+ 
+ Note that when calling this function the compiler may not be able to infer
+ the type that you are casting to. Use a type annotation to specify the type
+ that you expect.
+ 
+ - parameter pointer: The `UnsafePointer<Void>` that you wish to cast.
+ 
+ - returns: An object of type `T`.
+ 
+ */
 
 public func bridgeTransfer<T: AnyObject>(ptr: UnsafePointer<Void>) -> T {
     return Unmanaged<T>.fromOpaque(COpaquePointer(ptr)).takeRetainedValue()
--- Source/CodedPropertyType.swift
+++ Source/CodedPropertyType.swift
@@ -10,44 +10,77 @@
 
 /**
  
- A protocol for API-defined properties that are represented by a
+ A protocol for working with API-defined properties that are represented by a
  constant `UInt32` value.
  
  **Properties:**
  
- `var rawValue: UInt32 { get }`
- 
- By default, this is the same as the `code` property.
- 
- `var code: UInt32 { get }`
+     var code: UInt32 { get }
  
  The numeric value of the constant that represents the property. The
  'FourCharCode' for the value can be accessed through its `codeString` property.
  
- `var domain: String { get }`
+     var domain: String { get }
  
  The domain should be the API that defines the property. For example, the
  properties defined in System Sound Services will have the domain 'System
  Sound Services Property'.
  
- `var shortDescription: String { get }`
+     var shortDescription: String { get }
  
  A short description of the property.
- 
- `var description: String { get }`
- 
- The printable, formatted description of the property. The `description`
- will include information about the `domain`,
- the `shortDescription` and the `code` properties. If the `code` property
- represents a valid 'FourCharCode', the `codeString` will also be printed.
  
  */
 
 public protocol CodedPropertyType: CustomStringConvertible, RawRepresentable {
+    
+    /**
+     
+     The `rawValue` of the property.
+     
+     The default implementation returns the same value as `code` and is
+     always a `UInt32` value.
+     
+     */
+    
     var rawValue: UInt32 { get }
+    
+    /**
+     
+     The numeric value of the constant that represents the property. 
+     
+     The 'FourCharCode' for the value can be accessed through its 
+     `codeString` property.
+    
+     */
+    
     var code: UInt32 { get }
+    
+    /**
+    
+     A `String` that identifies the API that defines the property.
+    
+     For example, the properties defined in System Sound Services 
+     should have the domain 'System Sound Services Property'.
+    
+     */
+    
     var domain: String { get }
+    
+    /**
+    
+     A short description of the property.
+    
+     */
+    
     var shortDescription: String { get }
+    
+    /**
+    
+     Initialize a `CodedPropertyType` from a `UInt32` value.
+    
+     */
+    
     init?(code: UInt32)
 }
 
